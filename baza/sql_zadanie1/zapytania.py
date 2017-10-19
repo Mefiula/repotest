b75c69e3f00cf9694ca84b3a5dbf5f91ebfff18c
Mefiula <matkod1@wp.pl>
2017-10-19 08:41:42 +0200

    BÓBR INFORMATYCZNY

--- baza/sql_zadanie1/zapytania.py
+++ baza/sql_zadanie1/zapytania.py
@@ -5,6 +5,10 @@
 
 import sqlite3
 
+def wyniki(dane):
+    for rekord in dane:
+        print(tuple(rekord))
+        
 def kw_c(cur):
     cur.execute("""
         SELECT siedziba, SUM(placa) AS pensje 
@@ -14,21 +18,18 @@ def kw_c(cur):
         ORDER BY pensje ASC
     """)
     
-    wyniki = cur.fetchall()
-    for rekord in wyniki:
-        print(tuple(rekord))
+    wyniki(cur.fetchall())
+    
 
 
 def kw_e(cur):
     cur.execute("""
-        SELECT nazwisko, stanowisko, premia.premia
+        SELECT nazwisko, stanowisko, placa * premia.premia
         FROM premia, pracownicy
         WHERE premia.id = pracownicy.stanowisko
     """)
     
-    wyniki = cur.fetchall()
-    for rekord in wyniki:
-        print(tuple(rekord))
+    wyniki(cur.fetchall())
 
 
 
@@ -42,16 +43,53 @@ def kw_d(cur):
         AND imie NOT LIKE '%a'
     """, (parametr,))
     
-    wyniki = cur.fetchall()
-    for rekord in wyniki:
-        print(tuple(rekord))
+    wyniki(cur.fetchall())
 
+def kw_f(cur):
+    #~cur.execute("""
+        #~SELECT SUM(placa) / COUNT(imie)
+        #~FROM  pracownicy
+        #~WHERE imie Like '%a'
+    #~""")
+    
+    # NOT LIKE dla mężczyzn
+    
+    cur.execute("""
+        SELECT AVG(placa)
+        FROM  pracownicy
+        GROUP BY imie LIKE '%a'
+    """)
+    
+    wyniki(cur.fetchall())
+
+
+def kw_g(cur):
+    cur.execute("""
+        SELECT imie, nazwisko,
+        CAST ((JulianDay() - JulianDay(data_zatr)) / 365 AS Integer) AS okres
+        FROM pracownicy
+        ORDER BY okres DESC
+        LIMIT 0, 10
+    """)
+    
+    wyniki(cur.fetchall())
+
+def kw_h(cur):
+    parametr = input('Podaj numer działu: ')
+    cur.execute("""
+        SELECT imie, nazwisko, stanowisko, siedziba
+        FROM pracownicy, dzial
+        WHERE dzial.id = pracownicy.id_dzial
+        AND dzial.id = ?
+    """, (parametr,))
+    
+    wyniki(cur.fetchall())
 
 def main(args):
     con = sqlite3.connect('pracownicy.sqlite3')
     cur = con.cursor()  # utworzenie kursora
     
-    kw_e(cur)
+    kw_h(cur)
     
     return 0
 
